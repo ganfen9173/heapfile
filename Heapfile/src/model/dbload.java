f8f76e8f83b86ea4df925731f46148f10b10b70e
s3703529 <ganfen9173@hotmail.com>
2019-04-07 17:32:05 +1000

    dbload command

--- Heapfile/src/model/dbload.java
+++ Heapfile/src/model/dbload.java
@@ -1,14 +1,55 @@
 package model;
 
-public class dbload {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-	public dbload() {
-		// TODO Auto-generated constructor stub
-	}
+public class dbload {
+	static boolean printing = false;
+	static String csvFilename = "";
+	static int pageSize = 0;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Boolean aborting =false;
+		// parse args
+		for (int i =0; i< args.length && !aborting; i++) {
+			//
+			if (args.length < 3) {
+				System.exit(-1);
+			}else if (args[i].compareTo("-p") ==0 ) {
+				printing = true;
+/*				BufferedReader br = null;
+				try {
+					br = new BufferedReader(new FileReader("sample.csv"));
+				} catch (IOException e) {
+					System.out.println("Exception when trying to read file ");
+					e.printStackTrace();
+					
+				} finally {
+					try {
+						if (br != null) br.close();
+					} catch (IOException ex) {
+						System.out.println("Exception when trying to close BufferedReader:");
+						ex.printStackTrace();
+					}
+				}*/
+			} else if( i == 1 && pageSize == 0){
+				pageSize = Integer.parseInt(args[i]);
+			} else if (i == 2  && csvFilename.isEmpty()) {
+				csvFilename = args[i];
+			}
+		}
+		if (csvFilename.isEmpty()) {
+			System.err.println("ERROR: No argument found to specify csv file");
+			aborting =true;
+		}
+		if (pageSize==0) {
+			System.err.println("ERROR: No argument found to specify page size");
+			aborting =true;
+		}
+		if(!aborting) {
+			
+		}
 	}
 
 }
--- Heapfile/src/model/dbquery.java
+++ Heapfile/src/model/dbquery.java
@@ -4,7 +4,12 @@ public class dbquery {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String p = "-p";
+		if (p.compareTo("-p")==0) {
+			System.out.println("0");
+		}else {
+			System.out.println("1");
+		}
 	}
 
 }
