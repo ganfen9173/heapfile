8bc26981ba0f170d9527e118f7803274e14a6154
s3703529 <ganfen9173@hotmail.com>
2019-04-07 20:03:10 +1000

    update dbload

--- Heapfile/src/model/dbload.java
+++ Heapfile/src/model/dbload.java
@@ -9,33 +9,66 @@ public class dbload {
 	static String csvFilename = "";
 	static int pageSize = 0;
 
+	public void loadFile() {
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(csvFilename));
+			br.readLine();
+			//skip first line
+			String firstLine = "";
+			while ((firstLine = br.readLine()) != null){ //loop will run from 2nd line
+				
+			}
+			
+		} catch (IOException e) {
+			System.out.println("Exception when trying to read file ");
+			e.printStackTrace();		
+		} finally {
+			try {
+				if (br != null) br.close();
+			} catch (IOException ex) {
+				System.out.println("Exception when trying to close BufferedReader:");
+				ex.printStackTrace();
+			}
+		}
+	}
+	
+	public void loadRecords() {
+		
+	}
+	
+	// check if pagesize is a valid integer
+	   public static boolean isInteger(String s)
+	   {
+	      boolean isValidInt = false;
+	      try
+	      {
+	         Integer.parseInt(s);
+	         isValidInt = true;
+	      }
+	      catch (NumberFormatException e)
+	      {
+	    	 System.err.println("Exception when trying to canvert page size");
+	         e.printStackTrace();
+	      }
+	      return isValidInt;
+	   }
+	
 	public static void main(String[] args) {
+		long startTime = System.currentTimeMillis();
 		Boolean aborting =false;
 		// parse args
 		for (int i =0; i< args.length && !aborting; i++) {
 			//
 			if (args.length < 3) {
 				System.exit(-1);
-			}else if (args[i].compareTo("-p") ==0 ) {
+			} else if (args[i].compareTo("-p") ==0 ) {
 				printing = true;
-/*				BufferedReader br = null;
-				try {
-					br = new BufferedReader(new FileReader("sample.csv"));
-				} catch (IOException e) {
-					System.out.println("Exception when trying to read file ");
-					e.printStackTrace();
-					
-				} finally {
-					try {
-						if (br != null) br.close();
-					} catch (IOException ex) {
-						System.out.println("Exception when trying to close BufferedReader:");
-						ex.printStackTrace();
-					}
-				}*/
 			} else if( i == 1 && pageSize == 0){
+				if (isInteger(args[i])) {
 				pageSize = Integer.parseInt(args[i]);
-			} else if (i == 2  && csvFilename.isEmpty()) {
+				}
+			} else if (i == 2 && csvFilename.isEmpty()) {
 				csvFilename = args[i];
 			}
 		}
