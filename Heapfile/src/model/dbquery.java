3624589088a58abb6ce86de208eeaf22a367277f
DELL <ganfen9173@hotmail.com>
2019-04-11 19:44:16 +1000

    query structure update

--- Heapfile/src/model/dbquery.java
+++ Heapfile/src/model/dbquery.java
@@ -2,13 +2,55 @@ package model;
 
 public class dbquery {
 
+	static String searchString = "";
+	static int pageSize = 0;
+	
+	public static void searchFile() {
+		
+	}
+	
+	public static boolean isInteger(String s)
+	   {
+	      boolean isValidInt = false;
+	      try
+	      {
+	         Integer.parseInt(s);
+	         isValidInt = true;
+	      }
+	      catch (NumberFormatException e)
+	      {
+	    	 System.err.println("Exception when trying to canvert page size");
+	         e.printStackTrace();
+	      }
+	      return isValidInt;
+	   }
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String p = "-p";
-		if (p.compareTo("-p")==0) {
-			System.out.println("0");
-		}else {
-			System.out.println("1");
+		long startTime = System.currentTimeMillis();
+		Boolean aborting =false;
+		// parse args
+		for (int i =0; i< args.length && !aborting; i++) {
+			//
+			if (args.length < 2) {
+				System.exit(-1);
+			} else if (i == 0 && searchString.isEmpty()) {
+				searchString = args[i];
+			}else if( i == 1 && pageSize == 0){
+				if (isInteger(args[i])) {
+				pageSize = Integer.parseInt(args[i]);
+				}
+			}
+		}
+		if (searchString.isEmpty()) {
+			System.err.println("ERROR: No argument found to specify text search");
+			aborting =true;
+		}
+		if (pageSize==0) {
+			System.err.println("ERROR: No argument found to specify page size");
+			aborting =true;
+		}
+		if(!aborting) {
+			searchFile();
 		}
 	}
 
